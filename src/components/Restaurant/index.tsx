467c26444dcee30cfcf8f6030b79a94a8ca49cdc
Guilherme Oliveira <gui.live012@gmail.com>
2025-03-31 19:52:13 -0300

    fix: ajustes na formatação de endereços para navegação

--- src/components/Restaurant/index.tsx
+++ src/components/Restaurant/index.tsx
@@ -9,6 +9,7 @@ import {
 import Tag from '../Tag'
 import star from '../../assets/star.png'
 import { ButtonProduct } from '../Button/styles'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
   category: string[]
@@ -24,24 +25,41 @@ const RestaurantModel = ({
   image,
   title,
   score
-}: Props) => (
-  <Card>
-    <img src={image} alt={title} />
-    <Category>
-      {category.map((category) => (
-        <Tag key={category}>{category}</Tag>
-      ))}
-    </Category>
-    <TitleLine>
-      <Title>{title}</Title>
-      <ScoreContainer>
-        <p>{score}</p>
-        <img src={star} alt="Star rating" />
-      </ScoreContainer>
-    </TitleLine>
-    <Description>{description}</Description>
-    <ButtonProduct>Saiba mais</ButtonProduct>
-  </Card>
-)
+}: Props) => {
+  const navigate = useNavigate() // Hook para navegação
+
+  const formatTitleForURL = (title: string) => {
+    return title
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+  }
+
+  const handleRedirect = () => {
+    const formattedTitle = formatTitleForURL(title)
+    navigate(`/${formattedTitle}`)
+  }
+
+  return (
+    <Card>
+      <img src={image} alt={title} />
+      <Category>
+        {category.map((category) => (
+          <Tag key={category}>{category}</Tag>
+        ))}
+      </Category>
+      <TitleLine>
+        <Title>{title}</Title>
+        <ScoreContainer>
+          <p>{score}</p>
+          <img src={star} alt="Star rating" />
+        </ScoreContainer>
+      </TitleLine>
+      <Description>{description}</Description>
+      <ButtonProduct onClick={handleRedirect}>Saiba mais</ButtonProduct>
+    </Card>
+  )
+}
 
 export default RestaurantModel
